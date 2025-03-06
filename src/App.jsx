2d395726573f07271b74a1b5777fc1a09ca4f1cd
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-03-07 01:51:41 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,60 @@
 import { useState } from 'react'
 import './App.css'
 import { URL } from './constants';
+import Answer from './components/Answers';
 
 function App() {
-  const [question,setQuestion]=useState('');
-  const [result,setResult]=useState(undefined)
+  const [question, setQuestion] = useState('');
+  const [result, setResult] = useState(undefined)
 
- const payload ={
+  const payload = {
     "contents": [{
-    "parts":[{"text": question}]
+      "parts": [{ "text": question }]
     }]
-   }
-  
-  const askQuestion= async()=>{
-   let response = await fetch(URL,{
-    method:"POST",
-    body:JSON.stringify(payload)
-   })
-    
-   response = await response.json();
+  }
 
-  //  console.log(response.candidates[0].content.parts[0].text);
-   setResult(response.candidates[0].content.parts[0].text)
-   
+  const askQuestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload)
+    })
+
+    response = await response.json();
+    let dataString = response.candidates[0].content.parts[0].text;
+    dataString = dataString.split("* ");
+    dataString = dataString.map((item) => item.trim())
+
+    // console.log(dataString);
+    setResult(dataString)
+
   }
 
   return (
-   <div className='grid grid-cols-5 h-screen text-center'>
-    <div className='col-span-1 bg-zinc-800'>
-    </div>
-    <div className='col-span-4 p-10'>
-      <div className='container h-110 overflow-scroll'>
-       <div className='text-white'>
-       {result}
-       </div>
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
       </div>
-      <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
+      <div className='col-span-4 p-10'>
+        <div className='container h-110 overflow-scroll'>
+          <div className='text-zinc-300'>
+            <ul>
+              {/* {result} */}
+              {
+                result && result.map((item, index) => (
+                  <li key={index} className='text-left p-1'><Answer ans={item} totalResult={result.length} index={index} /></li>
+                ))
+              }
+
+            </ul>
+
+          </div>
+        </div>
+        <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
       border border-zinc-700 flex h-16'>
-        <input type="text" value={question} onChange={(event)=>setQuestion(event.target.value)} className='w-full h-full p-3 outline-none ' placeholder='Ask me anything' />
-        <button onClick={askQuestion} >Ask</button>
+          <input type="text" value={question} onChange={(event) => setQuestion(event.target.value)} className='w-full h-full p-3 outline-none ' placeholder='Ask me anything' />
+          <button onClick={askQuestion} >Ask</button>
+        </div>
       </div>
     </div>
-   </div>
   )
 }
 
---
+++ src/components/Answers.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { checkHeading, replaceHeadingStarts } from "../helper";
+
+const Answer = ({ ans, totalResult, index }) => {
+
+    const [heading, setHeading] = useState(false);
+    const [answer, setAnswer] = useState(ans);
+    console.log(index);
+
+
+    useEffect(() => {
+        if (checkHeading(ans)) {
+            setHeading(true);
+            setAnswer(replaceHeadingStarts(ans))
+        }
+
+    }, [])
+
+
+
+
+    return (
+        <>
+            {
+                index == 0 && totalResult > 1 ? <span className="pt-2 text-xl block text-white">{answer}</span> :
+                    heading ? <span className={"pt-2 text-lg block text-white"} >{answer}</span>
+                        : <span className="pl-5" >{answer}</span>
+            }
+
+
+        </>
+    )
+}
+
+export default Answer
