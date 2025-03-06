3661f22400bb1de14ea9ad53679f12c39525dc7a
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-03-06 17:45:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
 
+import { useState } from 'react'
 import './App.css'
+import { URL } from './constants';
 
 function App() {
+  const [question,setQuestion]=useState('');
+  const [result,setResult]=useState(undefined)
+
+ const payload ={
+    "contents": [{
+    "parts":[{"text": question}]
+    }]
+   }
+  
+  const askQuestion= async()=>{
+   let response = await fetch(URL,{
+    method:"POST",
+    body:JSON.stringify(payload)
+   })
+    
+   response = await response.json();
+
+  //  console.log(response.candidates[0].content.parts[0].text);
+   setResult(response.candidates[0].content.parts[0].text)
+   
+  }
 
   return (
    <div className='grid grid-cols-5 h-screen text-center'>
     <div className='col-span-1 bg-zinc-800'>
     </div>
     <div className='col-span-4 p-10'>
-      <div className='container h-110'>
-
+      <div className='container h-110 overflow-scroll'>
+       <div className='text-white'>
+       {result}
+       </div>
       </div>
       <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
       border border-zinc-700 flex h-16'>
-        <input type="text" className='w-full h-full p-3 outline-none ' placeholder='Ask me anything' />
-        <button>Ask</button>
+        <input type="text" value={question} onChange={(event)=>setQuestion(event.target.value)} className='w-full h-full p-3 outline-none ' placeholder='Ask me anything' />
+        <button onClick={askQuestion} >Ask</button>
       </div>
     </div>
    </div>
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDT8zHztGQIHtmn8CgNYiIJsECfkHUX6FQ"
