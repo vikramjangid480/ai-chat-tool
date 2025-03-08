30fdece58b6122b4e16d439d7d716db6b4a6599d
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-03-08 16:28:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Answer from './components/Answers';
 
 function App() {
   const [question, setQuestion] = useState('');
-  const [result, setResult] = useState([])
-  const id = useId()
+  const [result, setResult] = useState([]);
+  const [recentHistory, setRecentHistory] = useState(JSON.parse(localStorage.getItem('history')))
 
   const payload = {
     "contents": [{
@@ -16,6 +16,17 @@ function App() {
   }
 
   const askQuestion = async () => {
+
+    if (localStorage.getItem('history')) {
+      let history = JSON.parse(localStorage.getItem('history'))
+      history = [question, ...history]
+      localStorage.setItem('history', JSON.stringify(history))
+      setRecentHistory(history)
+    } else {
+      localStorage.setItem('history', JSON.stringify([question]))
+      setRecentHistory([question])
+    }
+
     let response = await fetch(URL, {
       method: "POST",
       body: JSON.stringify(payload)
@@ -27,40 +38,47 @@ function App() {
     dataString = dataString.map((item) => item.trim())
 
     // console.log(dataString);
-    setResult([...result,{type:'q',text:question},{type:'a',text:dataString}])
+    setResult([...result, { type: 'q', text: question }, { type: 'a', text: dataString }])
 
   }
 
-console.log(result);
+  console.log(recentHistory);
 
   return (
     <div className='grid grid-cols-5 h-screen text-center'>
       <div className='col-span-1 bg-zinc-800'>
+        <ul>
+          {
+            recentHistory && recentHistory.map((item)=>(
+              <li>{item}</li>
+            ))
+          }
+        </ul>
       </div>
       <div className='col-span-4 p-10'>
         <div className='container h-110 overflow-scroll'>
           <div className='text-zinc-300'>
             <ul>
-            {
-              result.map((item,index)=>(
-               <div key={index+Math.random()} className={item.type=='q'?'flex justify-end':''}>
-                {
-                   item.type=='q'? 
-                   <li key={index+Math.random()} 
-                   className='text-right p-1 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit
+              {
+                result.map((item, index) => (
+                  <div key={index + Math.random()} className={item.type == 'q' ? 'flex justify-end' : ''}>
+                    {
+                      item.type == 'q' ?
+                        <li key={index + Math.random()}
+                          className='text-right p-1 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit
                    '
-                   ><Answer ans={item.text} totalResult={1} index={index} type={item.type}  /></li>
-                   :item.text.map((ansItem,ansIndex)=>(
-                   <li key={ansIndex+Math.random()} className='text-left p-1'><Answer ans={ansItem} totalResult={item.length} type={item.type} index={ansIndex} /></li>
- 
-                   ))
-                }
-               </div>
-               
-              ))
-            }
+                        ><Answer ans={item.text} totalResult={1} index={index} type={item.type} /></li>
+                        : item.text.map((ansItem, ansIndex) => (
+                          <li key={ansIndex + Math.random()} className='text-left p-1'><Answer ans={ansItem} totalResult={item.length} type={item.type} index={ansIndex} /></li>
+
+                        ))
+                    }
+                  </div>
+
+                ))
+              }
             </ul>
-         
+
 
           </div>
         </div>
