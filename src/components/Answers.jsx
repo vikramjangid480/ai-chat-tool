ede71e6f303836b8e4fc2a219dedd99857b5c279
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-03-08 15:01:48 +0530

    done

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { checkHeading, replaceHeadingStarts } from "../helper";
 
-const Answer = ({ ans, totalResult, index }) => {
+const Answer = ({ ans, totalResult, index,type }) => {
 
     const [heading, setHeading] = useState(false);
     const [answer, setAnswer] = useState(ans);
@@ -23,7 +23,7 @@ const Answer = ({ ans, totalResult, index }) => {
             {
                 index == 0 && totalResult > 1 ? <span className="pt-2 text-xl block text-white">{answer}</span> :
                     heading ? <span className={"pt-2 text-lg block text-white"} >{answer}</span>
-                        : <span className="pl-5" >{answer}</span>
+                        : <span className={type=='q'?'pl-1':'pl-5'} >{answer}</span>
             }
 
 
