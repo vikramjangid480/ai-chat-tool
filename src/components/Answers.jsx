d9f44821c7f125828db1257df44e32de9e5dfbdf
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-05-07 17:33:23 +0530

    done with highlight code

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { checkHeading, replaceHeadingStarts } from "../helper";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/cjs/light";
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import ReactMarkdown from 'react-markdown'
 
 const Answer = ({ ans, totalResult, index,type }) => {
 
@@ -15,15 +19,33 @@ const Answer = ({ ans, totalResult, index,type }) => {
 
     }, [])
 
-
-
+    const renderer = {
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              children={String(children).replace(/\n$/, '')}
+              language={match[1]}
+              style={dark}
+              PreTag="div"
+            />
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      };
 
     return (
         <>
             {
                 index == 0 && totalResult > 1 ? <span className="pt-2 text-xl block text-white">{answer}</span> :
                     heading ? <span className={"pt-2 text-lg block text-white"} >{answer}</span>
-                        : <span className={type=='q'?'pl-1':'pl-5'} >{answer}</span>
+                        : <span className={type=='q'?'pl-1':'pl-5'} >
+                            <ReactMarkdown components={renderer} >{answer}</ReactMarkdown>
+                        </span>
             }
 
 
